a5e3f0b6a65d8949bc409c3f58d7d2214aa9870d
Aleksandr <63979218+alexzhirkevich@users.noreply.github.com>
2021-04-19 19:57:03 +0300

    Delete Java/trash directory

--- Java/trash/lab5/Body.java
+++
@@ -1,7 +0,0 @@
-package lab5;
-import java.util.*;
-
-interface Body<T> extends Comparable<T>, Comparator<T>, Iterable<Object>, Iterator<Object> {
-    public double area();
-    public double volume();
-}
